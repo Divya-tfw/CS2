7df080ebdecc4a58bb42d39361f7659a4fe799f2
Divya <Divya>
2016-08-23 15:19:03 +0530

    Initial commit

---
+++ Android.bat
@@ -0,0 +1,2 @@
+cd D:/Auto_Tools/Sv6.3/userdata/CS2
+ant -DGroupName="null" -DSentMessage="null" -DscriptName=Android/TC_Chat.sah -f build.xml
---
+++ Gen2_Run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\qateam\sahi_pro\userdata\bin
+testrunner ../CS2/Suite/Gen2_DA.dd.csv  http://sahitest.com/demo/ chrome "Gen2_DomAdmin_C42473"
